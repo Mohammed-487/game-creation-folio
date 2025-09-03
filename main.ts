630fca3882968c6c0f9fe534fd2e0cc1e195a0d4
Mohammed-487 <mhahm9@schools.vic.edu.au>
2025-09-03 10:02:32 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    d d d d d d d d d d d d d d d d 
+    d d d d d d d d d d d d d d d d 
+    d d d d d d d d d d d d d d d d 
+    d d d d d d d d d d d d d d d d 
+    d d d d d d d d d d d d d d d d 
+    d d d d d d d d d d d d d d d d 
+    d d d d d d d d d d d d d d d d 
+    d d d d d d d d d d d d d d d d 
+    d d d d d d d d d d d d d d d d 
+    `, SpriteKind.Player)
